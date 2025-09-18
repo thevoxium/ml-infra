d5a984a625c0c26a61865889e94e5a5030d25d9e
thevoxium <thevoxium@gmail.com>
2025-09-19 01:33:30 +0530

    add Node class

---
+++ w1/graphdb.cpp
@@ -0,0 +1,77 @@
+#include <algorithm>
+#include <cstddef>
+#include <iostream>
+#include <memory>
+#include <queue>
+#include <stack>
+#include <stdexcept>
+#include <string>
+#include <sys/_types/_ssize_t.h>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+
+class Node;
+class Edge;
+class Graph;
+
+using PropertyValue = std::variant<int, std::string, bool>;
+using NodePtr = std::shared_ptr<Node>;
+using EdgePrt = std::shared_ptr<Edge>;
+using NodeId = size_t;
+using EdgeId = size_t;
+
+class Node {
+private:
+  NodeId id;
+  std::string label;
+  std::unordered_map<std::string, PropertyValue> properties;
+
+public:
+  Node(NodeId id, std::string label) {
+    this->id = id;
+    this->label = label;
+  }
+
+  Node(Node &other) = delete;
+  Node &operator=(Node &other) = delete;
+  Node(Node &&other) = default;
+  Node &operator=(Node &&other) = default;
+  ~Node() = default;
+
+  NodeId getId() const;
+  const std::string &getLabel() const;
+  const std::unordered_map<std::string, PropertyValue> &getMap() const;
+
+  void setProperty(const std::string &key, PropertyValue value);
+  bool hasProperty(const std::string &key) const;
+  const PropertyValue &getProperty(const std::string &key) const;
+};
+
+NodeId Node::getId() const { return this->id; }
+const std::string &Node::getLabel() const { return this->label; }
+const std::unordered_map<std::string, PropertyValue> &Node::getMap() const {
+  return this->properties;
+}
+
+void Node::setProperty(const std::string &key, PropertyValue value) {
+  this->properties[key] = std::move(value);
+}
+bool Node::hasProperty(const std::string &key) const {
+  return this->properties.find(key) == this->properties.end() ? false : true;
+}
+
+const PropertyValue &Node::getProperty(const std::string &key) const {
+  auto it = this->properties.find(key);
+  if (it == this->properties.end()) {
+    throw std::runtime_error("Property '" + key + "' not found");
+  }
+  return it->second;
+}
+
+int main() {
+  NodePtr newNode = std::make_shared<Node>(1, "anshul");
+  newNode->setProperty("age", 24);
+  std::cout << std::get<int>(newNode->getProperty("age")) << std::endl;
+  return 0;
+}
